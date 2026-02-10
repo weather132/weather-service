de904127beab3375be1b3f93486db536a55ce372
Yun531 <jaeyoongh315@gmail.com>
2026-02-10 17:44:02 +0900

    refactor(entity): ClimateSnap에서 validAt 필드 삭제 + seriesStartTime 추가

--- src/main/java/com/github/yun531/climate/entity/ClimateSnap.java
+++ src/main/java/com/github/yun531/climate/entity/ClimateSnap.java
@@ -3,15 +3,15 @@ package com.github.yun531.climate.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
 import jakarta.persistence.Table;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
-@Setter
 @Entity
+@IdClass(ClimateSnapId.class)
 @Table(name = "climate_snap")
 public class ClimateSnap {
 
@@ -19,39 +19,32 @@ public class ClimateSnap {
     @Column(name = "snap_id")
     private Integer snapId;
 
-    @Column(name = "region_id", nullable = false)
+    @Id
+    @Column(name = "region_id", nullable = false, length = 8)
     private String regionId;
 
     @Column(name = "report_time", nullable = false)    // todo : db에 넣을 때 fommating 해서 문자열로 넣느게 나을수도 있음
     private LocalDateTime reportTime;
 
-    // ---- 시간대별 발효시각 (01~26시) ----
-    @Column(name = "valid_at_A01") private LocalDateTime validAtA01; @Column(name = "valid_at_A02") private LocalDateTime validAtA02;
-    @Column(name = "valid_at_A03") private LocalDateTime validAtA03; @Column(name = "valid_at_A04") private LocalDateTime validAtA04;
-    @Column(name = "valid_at_A05") private LocalDateTime validAtA05; @Column(name = "valid_at_A06") private LocalDateTime validAtA06;
-    @Column(name = "valid_at_A07") private LocalDateTime validAtA07; @Column(name = "valid_at_A08") private LocalDateTime validAtA08;
-    @Column(name = "valid_at_A09") private LocalDateTime validAtA09; @Column(name = "valid_at_A10") private LocalDateTime validAtA10;
-    @Column(name = "valid_at_A11") private LocalDateTime validAtA11; @Column(name = "valid_at_A12") private LocalDateTime validAtA12;
-    @Column(name = "valid_at_A13") private LocalDateTime validAtA13; @Column(name = "valid_at_A14") private LocalDateTime validAtA14;
-    @Column(name = "valid_at_A15") private LocalDateTime validAtA15; @Column(name = "valid_at_A16") private LocalDateTime validAtA16;
-    @Column(name = "valid_at_A17") private LocalDateTime validAtA17; @Column(name = "valid_at_A18") private LocalDateTime validAtA18;
-    @Column(name = "valid_at_A19") private LocalDateTime validAtA19; @Column(name = "valid_at_A20") private LocalDateTime validAtA20;
-    @Column(name = "valid_at_A21") private LocalDateTime validAtA21; @Column(name = "valid_at_A22") private LocalDateTime validAtA22;
-    @Column(name = "valid_at_A23") private LocalDateTime validAtA23; @Column(name = "valid_at_A24") private LocalDateTime validAtA24;
-    @Column(name = "valid_at_A25") private LocalDateTime validAtA25; @Column(name = "valid_at_A26") private LocalDateTime validAtA26;
+    @Column(name = "series_start_time", nullable = false)
+    private LocalDateTime seriesStartTime;
 
     // ---- 시간대별 온도 (01~26시) ----
     @Column(name = "temp_A01") private Integer tempA01; @Column(name = "temp_A02") private Integer tempA02;
-    @Column(name = "temp_A03") private Integer tempA03; @Column(name = "temp_A04") private Integer tempA04; @Column(name = "temp_A05") private Integer tempA05;
-    @Column(name = "temp_A06") private Integer tempA06; @Column(name = "temp_A07") private Integer tempA07; @Column(name = "temp_A08") private Integer tempA08;
-    @Column(name = "temp_A09") private Integer tempA09; @Column(name = "temp_A10") private Integer tempA10; @Column(name = "temp_A11") private Integer tempA11;
-    @Column(name = "temp_A12") private Integer tempA12; @Column(name = "temp_A13") private Integer tempA13; @Column(name = "temp_A14") private Integer tempA14;
-    @Column(name = "temp_A15") private Integer tempA15; @Column(name = "temp_A16") private Integer tempA16; @Column(name = "temp_A17") private Integer tempA17;
-    @Column(name = "temp_A18") private Integer tempA18; @Column(name = "temp_A19") private Integer tempA19; @Column(name = "temp_A20") private Integer tempA20;
-    @Column(name = "temp_A21") private Integer tempA21; @Column(name = "temp_A22") private Integer tempA22; @Column(name = "temp_A23") private Integer tempA23;
-    @Column(name = "temp_A24") private Integer tempA24; @Column(name = "temp_A25") private Integer tempA25; @Column(name = "temp_A26") private Integer tempA26;
+    @Column(name = "temp_A03") private Integer tempA03; @Column(name = "temp_A04") private Integer tempA04;
+    @Column(name = "temp_A05") private Integer tempA05; @Column(name = "temp_A06") private Integer tempA06;
+    @Column(name = "temp_A07") private Integer tempA07; @Column(name = "temp_A08") private Integer tempA08;
+    @Column(name = "temp_A09") private Integer tempA09; @Column(name = "temp_A10") private Integer tempA10;
+    @Column(name = "temp_A11") private Integer tempA11; @Column(name = "temp_A12") private Integer tempA12;
+    @Column(name = "temp_A13") private Integer tempA13; @Column(name = "temp_A14") private Integer tempA14;
+    @Column(name = "temp_A15") private Integer tempA15; @Column(name = "temp_A16") private Integer tempA16;
+    @Column(name = "temp_A17") private Integer tempA17; @Column(name = "temp_A18") private Integer tempA18;
+    @Column(name = "temp_A19") private Integer tempA19; @Column(name = "temp_A20") private Integer tempA20;
+    @Column(name = "temp_A21") private Integer tempA21; @Column(name = "temp_A22") private Integer tempA22;
+    @Column(name = "temp_A23") private Integer tempA23; @Column(name = "temp_A24") private Integer tempA24;
+    @Column(name = "temp_A25") private Integer tempA25; @Column(name = "temp_A26") private Integer tempA26;
 
-    // ---- 일자별 오전/오후 온도 ----
+    // ---- 일자별 최저/최고 온도 ----
     @Column(name = "temp_A0d_min") private Integer tempA0dMin; @Column(name = "temp_A0d_max") private Integer tempA0dMax;
     @Column(name = "temp_A1d_min") private Integer tempA1dMin; @Column(name = "temp_A1d_max") private Integer tempA1dMax;
     @Column(name = "temp_A2d_min") private Integer tempA2dMin; @Column(name = "temp_A2d_max") private Integer tempA2dMax;
@@ -60,17 +53,20 @@ public class ClimateSnap {
     @Column(name = "temp_A5d_min") private Integer tempA5dMin; @Column(name = "temp_A5d_max") private Integer tempA5dMax;
     @Column(name = "temp_A6d_min") private Integer tempA6dMin; @Column(name = "temp_A6d_max") private Integer tempA6dMax;
 
-
     // ---- 시간대별 강수확률 (01~26시) ----
     @Column(name = "POP_A01") private Integer popA01; @Column(name = "POP_A02") private Integer popA02;
-    @Column(name = "POP_A03") private Integer popA03; @Column(name = "POP_A04") private Integer popA04; @Column(name = "POP_A05") private Integer popA05;
-    @Column(name = "POP_A06") private Integer popA06; @Column(name = "POP_A07") private Integer popA07; @Column(name = "POP_A08") private Integer popA08;
-    @Column(name = "POP_A09") private Integer popA09; @Column(name = "POP_A10") private Integer popA10; @Column(name = "POP_A11") private Integer popA11;
-    @Column(name = "POP_A12") private Integer popA12; @Column(name = "POP_A13") private Integer popA13; @Column(name = "POP_A14") private Integer popA14;
-    @Column(name = "POP_A15") private Integer popA15; @Column(name = "POP_A16") private Integer popA16; @Column(name = "POP_A17") private Integer popA17;
-    @Column(name = "POP_A18") private Integer popA18; @Column(name = "POP_A19") private Integer popA19; @Column(name = "POP_A20") private Integer popA20;
-    @Column(name = "POP_A21") private Integer popA21; @Column(name = "POP_A22") private Integer popA22; @Column(name = "POP_A23") private Integer popA23;
-    @Column(name = "POP_A24") private Integer popA24; @Column(name = "POP_A25") private Integer popA25; @Column(name = "POP_A26") private Integer popA26;
+    @Column(name = "POP_A03") private Integer popA03; @Column(name = "POP_A04") private Integer popA04;
+    @Column(name = "POP_A05") private Integer popA05; @Column(name = "POP_A06") private Integer popA06;
+    @Column(name = "POP_A07") private Integer popA07; @Column(name = "POP_A08") private Integer popA08;
+    @Column(name = "POP_A09") private Integer popA09; @Column(name = "POP_A10") private Integer popA10;
+    @Column(name = "POP_A11") private Integer popA11; @Column(name = "POP_A12") private Integer popA12;
+    @Column(name = "POP_A13") private Integer popA13; @Column(name = "POP_A14") private Integer popA14;
+    @Column(name = "POP_A15") private Integer popA15; @Column(name = "POP_A16") private Integer popA16;
+    @Column(name = "POP_A17") private Integer popA17; @Column(name = "POP_A18") private Integer popA18;
+    @Column(name = "POP_A19") private Integer popA19; @Column(name = "POP_A20") private Integer popA20;
+    @Column(name = "POP_A21") private Integer popA21; @Column(name = "POP_A22") private Integer popA22;
+    @Column(name = "POP_A23") private Integer popA23; @Column(name = "POP_A24") private Integer popA24;
+    @Column(name = "POP_A25") private Integer popA25; @Column(name = "POP_A26") private Integer popA26;
 
     // ---- 일자별 오전/오후 강수확률 ----
     @Column(name = "POP_A0d_am") private Integer popA0dAm; @Column(name = "POP_A0d_pm") private Integer popA0dPm;
--- src/main/java/com/github/yun531/climate/repository/ClimateSnapRepository.java
+++ src/main/java/com/github/yun531/climate/repository/ClimateSnapRepository.java
@@ -1,53 +1,19 @@
 package com.github.yun531.climate.repository;
 
-import com.github.yun531.climate.repository.dto.POPSnapDto;
 import com.github.yun531.climate.entity.ClimateSnap;
+import com.github.yun531.climate.entity.ClimateSnapId;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ClimateSnapRepository extends JpaRepository<ClimateSnap, Integer> {
+public interface ClimateSnapRepository extends JpaRepository<ClimateSnap, ClimateSnapId> {
 
-    // snap_id로 특정 데이터 조회
+    // snap_id로 특정 데이터 조회 (여러 region 포함될 수 있음)
     List<ClimateSnap> findBySnapIdIn(List<Integer> snapIds);
 
     // region_id로 조회
     List<ClimateSnap> findByRegionId(String regionId);
 
-    // snap_id와 region_id를 함께 조건으로 조회
+    // snap_id + region_id
     ClimateSnap findBySnapIdAndRegionId(Integer snapId, String regionId);
-
-    @Query("""
-        SELECT new com.github.yun531.climate.repository.dto.POPSnapDto(
-            c.snapId, c.regionId, c.reportTime,
-
-            c.validAtA01, c.validAtA02, c.validAtA03, c.validAtA04, c.validAtA05,
-            c.validAtA06, c.validAtA07, c.validAtA08, c.validAtA09, c.validAtA10,
-            c.validAtA11, c.validAtA12, c.validAtA13, c.validAtA14, c.validAtA15,
-            c.validAtA16, c.validAtA17, c.validAtA18, c.validAtA19, c.validAtA20,
-            c.validAtA21, c.validAtA22, c.validAtA23, c.validAtA24, c.validAtA25, c.validAtA26,
-
-            c.popA01, c.popA02, c.popA03, c.popA04, c.popA05,
-            c.popA06, c.popA07, c.popA08, c.popA09, c.popA10,
-            c.popA11, c.popA12, c.popA13, c.popA14, c.popA15,
-            c.popA16, c.popA17, c.popA18, c.popA19, c.popA20,
-            c.popA21, c.popA22, c.popA23, c.popA24, c.popA25, c.popA26,
-
-            c.popA0dAm, c.popA0dPm,
-            c.popA1dAm, c.popA1dPm,
-            c.popA2dAm, c.popA2dPm,
-            c.popA3dAm, c.popA3dPm,
-            c.popA4dAm, c.popA4dPm,
-            c.popA5dAm, c.popA5dPm,
-            c.popA6dAm, c.popA6dPm
-        )
-        FROM ClimateSnap c
-        WHERE c.snapId IN :snapIds and c.regionId = :regionId
-    """)
-    List<POPSnapDto> findPopInfoBySnapIdsAndRegionId(
-            @Param("snapIds") List<Integer> snapIds,
-            @Param("regionId") String regionId
-    );
 }
